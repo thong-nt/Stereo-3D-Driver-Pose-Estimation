60bf87aeffa42354a9be02ca356b106db0545815
ThongNguyen <trithong2103@gmail.com>
2021-12-29 17:28:49 +0200

    Check

--- modules/pose.py
+++ modules/pose.py
@@ -36,8 +36,6 @@ class Pose:
             found_keypoints[found_kpt_id] = keypoints[kpt_id]
             found_kpt_id += 1
         bbox = cv2.boundingRect(found_keypoints)
-
-        print("Length of bbox", len(bbox))
         return bbox
 
     def update_id(self, id=None):
