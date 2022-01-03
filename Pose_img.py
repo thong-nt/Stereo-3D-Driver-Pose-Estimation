a499236ab83212399ce082a68bf984bf065fe2a2
ThongNguyen <trithong2103@gmail.com>
2022-01-03 23:29:26 +0200

    Test img

--- Pose_img.py
+++ Pose_img.py
@@ -20,7 +20,42 @@ def run_3dpose(net):
     upsample_ratio = 4
     num_keypoints = Pose.num_kpts
 
-    img = cv2.imread("img_34.jpg", cv2.IMREAD_COLOR)
+    imgloc = "img_34.jpg"
+    Rimg = cv2.imread(imgloc, cv2.IMREAD_COLOR)
+    Limg = cv2.imread(imgloc, cv2.IMREAD_COLOR)
+
+    pose_extraction_scheduler = PoseScheduler()
+    pose_infer_scheduler = PoseInferScheduler(net, stride, upsample_ratio, cpu)
+    pose_infer_scheduler.start_infer()
+
+    pose3d = None
+    Rimg = None
+    Limg = None
+    newpose, r_current_poses, l_current_poses, Rimg_synced, Limg_synced = False, None, None, None, None
+   
+    while True:
+        pose_infer_scheduler.set_images(Rimg, Limg)
+
+        Lnewpose, r_pose_data, Rimg_sync = pose_infer_scheduler.get_left_pose()
+        Rnewpose, l_pose_data, Limg_sync = pose_infer_scheduler.get_right_pose()
+
+        Rheatmaps, Rpafs, Rscale, Rpad = r_pose_data
+        Lheatmaps, Lpafs, Lscale, Lpad = l_pose_data
+
+        if pose_extraction_scheduler.is_done():
+            pose_extraction_scheduler.schedule_new_stereo_extract("left", Rimg_sync, r_previous_poses, Rheatmaps, Rpafs, Rscale, Rpad, num_keypoints, stride, upsample_ratio)
+            pose_extraction_scheduler.schedule_new_stereo_extract("right", Limg_sync, l_previous_poses, Lheatmaps, Lpafs, Lscale, Lpad, num_keypoints, stride, upsample_ratio)
+
+            newpose, r_current_poses, l_current_poses, Rimg_synced, Limg_synced = pose_extraction_scheduler.execute_schedule()
+
+        Rimg = draw_pose(r_current_poses, Rimg_synced)
+        Limg = draw_pose(l_current_poses, Limg_synced)
+
+        cv2.imshow('img', np.hstack([Rimg, Limg]))
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            pose_infer_scheduler.stop_infer()
+            return
 
 
 if __name__ == '__main__':
