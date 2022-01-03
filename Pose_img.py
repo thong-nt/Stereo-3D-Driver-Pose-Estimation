cc73c2ae21e51fd18dc7d81ec8d9b6f11c1dc848
ThongNguyen <trithong2103@gmail.com>
2022-01-03 22:47:37 +0200

    Test img

--- Pose_img.py
+++ Pose_img.py
@@ -5,7 +5,7 @@ import torch
 from models.with_mobilenet import PoseEstimationWithMobileNet
 from modules.keypoints import extract_keypoints, group_keypoints
 from modules.load_state import load_state
-from modules.pose import draw, track_poses
+from modules.pose import Pose, track_poses
 from val import normalize, pad_width
 
 from pose3dmodules import *
@@ -21,7 +21,7 @@ def run_3dpose(net):
         img = cv2.resize(img, (360, 270))
 
     orig_img = img
-    draw(img)
+    Pose.draw(img)
     img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
     cv2.imshow('image',img)
     key = cv2.waitKey(1)
