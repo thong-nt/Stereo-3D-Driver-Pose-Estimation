21fe6850713b8053c53c7f5717e912b3124a4902
ThongNguyen <trithong2103@gmail.com>
2022-01-04 16:54:59 +0200

    Test img

--- Pose_img.py
+++ Pose_img.py
@@ -61,7 +61,7 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
     if not cpu:
         net = net.cuda()
 
-    stride = 8
+    stride = 4
     upsample_ratio = 4
     num_keypoints = Pose.num_kpts
     previous_poses = []
